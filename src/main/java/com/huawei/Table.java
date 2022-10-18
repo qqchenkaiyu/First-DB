d0fcfb244b4bc5376d5d1de3bb35563b763ca8eb
qqchenkaiyu <37261937+qqchenkaiyu@users.noreply.github.com>
2022-10-18 20:28:14 +0800

    Update Table.java

--- src/main/java/com/huawei/Table.java
+++ src/main/java/com/huawei/Table.java
@@ -6,10 +6,10 @@ import com.huawei.index.HashIndex;
 import com.huawei.storage.DataItem;
 import com.huawei.storage.DataManager;
 
-import com.alibaba.fastjson.JSONObject;
+import com.google.common.collect.Lists;
 import com.google.common.primitives.Bytes;
 
-import cn.hutool.core.date.StopWatch;
+import cn.hutool.core.lang.Tuple;
 import cn.hutool.core.util.ByteUtil;
 import lombok.Data;
 
@@ -17,12 +17,15 @@ import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
-import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 @Data
 public class Table {
     private String tableName;
+
+    private Long rowNum;
 
     private LinkedList<Field> fields;
 
@@ -58,7 +61,7 @@ public class Table {
                 case "string":
                     String str = (String) entry.get(field.getFieldName());
                     byte[] bytes = str.getBytes();
-                    raw = Bytes.concat(raw, ByteUtil.shortToBytes((short) str.length()),bytes);
+                    raw = Bytes.concat(raw, ByteUtil.shortToBytes((short) str.length()), bytes);
                     break;
             }
         }
@@ -68,17 +71,21 @@ public class Table {
     public LinkedList<Map<String, Object>> select(Select select) throws Exception {
         // 判断是否走索引
         LinkedList<Map<String, Object>> maps = new LinkedList<>();
-        if(!select.whereMap.isEmpty()){
-            // 走索引
-            long id = select.getWhereMap().get("id").get(1);
-            Long offset = hashIndex.getIndex().get(id);
-            if(offset!=null){
-                DataItem dataItem = dataManager.read(offset);
-                Map<String, Object> entry = parseEntry(dataItem.getBuffer(), select);
-                maps.add(entry);
+        // 生成执行计划
+        //
+        Explain explain = generateExplain(select);
+        return handleByExplain(explain,select);
+    }
+
+    private LinkedList<Map<String, Object>> handleByExplain(Explain explain,Select select) throws Exception {
+        LinkedList<Map<String, Object>> maps = new LinkedList<>();
+        if(explain.getType().equals("system")){
+            DataItem dataItem = dataManager.read(0);
+            Map<String, Object> entry = parseEntry(dataItem.getBuffer(), select);
+            if (entry != null) {
+              maps.add(entry);
             }
-        }else{
-            // 走全盘扫描
+        }else if(explain.getType().equals("ALL")){
             dataManager.position(0);
             DataItem dataItem;
             while ((dataItem = dataManager.next()) != null) {
@@ -87,9 +94,46 @@ public class Table {
                     maps.add(entry);
                 }
             }
+        }else{
+            // 走索引
+            long id = select.getWhereMap().get(explain.getKey()).get(1);
+            Long offset = hashIndex.getIndex().get(id);
+            if (offset != null) {
+                DataItem dataItem = dataManager.read(offset);
+                Map<String, Object> entry = parseEntry(dataItem.getBuffer(), select);
+                maps.add(entry);
+            }
         }
         return maps;
+    }
 
+    private Explain generateExplain(Select select) {
+        Explain explain = new Explain();
+        if (rowNum == 1) {
+            explain.setType("system");
+            return explain;
+        }
+        List<Tuple> where = select.where;
+        LinkedList<Field> fields = this.fields;
+        List<Tuple> collect = where.stream().filter(tuple -> {
+            for (Field field1 : fields) {
+                if (field1.getFieldName().equals(tuple.get(0).toString())) {
+                    return field1.isIndexed();
+                }
+            }
+            return false;
+        }).collect(Collectors.toList());
+        if (collect.isEmpty()) {
+            // 没有找到索引
+            explain.setType("ALL");
+        } else {
+            // 暂时直接选第一个
+            Tuple objects = collect.get(0);
+            explain.setPossible_keys(Lists.transform(collect,tuple->tuple.get(0)).toString());
+            explain.setKey(objects.get(0));
+            explain.setType("const");
+        }
+        return explain;
     }
 
     // 处理一行数据
